8d9a049f8b03a2ebec6005875d900740adcd5ca2
Vinod Mathew Sebastian <vinod.sebastian@rediffmail.com>
2019-05-17 10:37:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/', function (req, res) {
 });
 
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+let ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 
